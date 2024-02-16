09ec0fa2ab44af291cec633e2691b0afa05724bf
Md Monirozzaman <mdmonirozzaman7@gmail.com>
2024-02-16 18:04:42 +0600

    Generat  random number and set that number as alphabet and set keyboard color by id.

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,4 +1,14 @@
+function playContinue() {
+    const alphabet = getARandomAlphabet();
+    
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    setbackgroundColotById(alphabet);
+}
+
+
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    playContinue();
 }
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,24 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setbackgroundColotById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefgtijklmnopqrstuvwayz';
+    const alphabets = alphabetString.split('');
+    
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
