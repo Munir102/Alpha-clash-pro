38656dea878a813bc3affe85d6176c75016f8014
Md Monirozzaman <mdmonirozzaman7@gmail.com>
2024-02-16 19:13:08 +0600

    Set life score and life. if press the suggest key then score will be mincrease and if press the whorng key then life wioll be decrease.

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -6,8 +6,39 @@ function playContinue() {
     setbackgroundColotById(alphabet);
 }
 
-document.addEventListener('keyup', function handleKeyboardButtonPress(){
-    console.log('Key press');
+document.addEventListener('keyup', function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabat = currentAlphabet;
+
+    if(playerPressed === expectedAlphabat){
+        // console.log("You got a point");
+        // get current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currenScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currenScoreText);
+
+        // increase score
+        const updateScore = currentScore + 1;
+
+        // show the update score
+        currentScoreElement.innerText = updateScore;
+
+        removeBackgroundColorById(expectedAlphabat);
+        playContinue();
+
+    }else{
+        // console.log('Please press correct key.');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLifeNum = parseInt(currentLifeText);
+
+        const updateLife = currentLifeNum - 1;
+
+        currentLifeElement.innerText = updateLife;
+    }
 })
 
 function play() {
